4ad10020643bad538a02a828152c2edf493193c4
Ray Donnelly <mingw.android@gmail.com>
2017-04-02 14:47:34 +0100

    Add support cross-compilation
    Testing is done via QEMU via Dejagnu and requires some special handling.

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,10 +11,21 @@ if [[ $(uname) == "Linux" ]]; then
   sed -i 's:@toolexeclibdir@:${libdir}:g' libffi.pc.in
 fi
 
-./configure --disable-debug --disable-dependency-tracking \
+./configure --build=${BUILD} --host=${HOST} \
+            --disable-debug --disable-dependency-tracking \
             --prefix="${PREFIX}" --includedir="${PREFIX}/include" \
   || { cat config.log; exit 1;}
 
 make
-make check
+
+if [[ -n "${QEMU_LD_PREFIX}" ]] && [[ "${HOST}" != "${BUILD}" ]]; then
+  # Yuck. Dejagnu should instead pass '-Wl,-rpath=\$ORIGIN/../.libs' when building
+  # tests or at least provide a way for us to pass that. In non-cross scenarios it
+  # sets LD_LIBRARY_PATH (see set_ld_library_path_env_vars), and has no provisions
+  # for QEMU at all.
+  QEMU_SET_ENV="LD_LIBRARY_PATH=${SRC_DIR}/${HOST}/.libs" make check
+else
+  make check
+fi
+
 make install
