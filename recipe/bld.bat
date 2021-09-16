6f64eb6a4d039123ab1059124b3f30c6be1539dc
Maxwell Varlack <varlackc@mail.gvsu.edu>
2021-09-16 12:20:43 -0500

    changes to use autotools_clang_conda

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,6 @@ FOR /F "delims=" %%i in ('cygpath.exe -u "LIBRARY_PREFIX"') DO set "PREFIX=%%i"
 FOR /F "delims=" %%i in ('cygpath.exe -u "BUILD_PREFIX"') DO set "BUILD_PREFIX=%%i"
 FOR /F "delims=" %%i in ('cygpath.exe -u "SRC_DIR"') DO set "SRC_DIR=%%i"
 FOR /F "delims=" %%i in ('cygpath.exe -u "RECIPE_DIR"') DO set "RECIPE_DIR=%%i"
-bash -lce build.sh
+bash -lc build.sh
 if %ERRORLEVEL% neq 0 exit 1
 copy %LIBRARY_LIB%\libffi.lib %LIBRARY_LIB%\ffi.lib
